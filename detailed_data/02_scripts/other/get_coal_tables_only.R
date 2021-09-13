16aa1d0c189b051604f0f817b73fbda0f7036d81
SimonJasansky <simon.jasansky@wu.ac.at>
2021-09-13 08:07:40 +0000

    added unfinished section to compile one large xlsx sheet with all relevant coal data (tidy format)

--- detailed_data/02_scripts/other/get_coal_tables_only.R
+++ detailed_data/02_scripts/other/get_coal_tables_only.R
@@ -41,3 +41,48 @@ write.xlsx(data.frame(reserves), file="./detailed_data/04_output/07_other/coal_t
 write.xlsx(data.frame(waste), file="./detailed_data/04_output/07_other/coal_tables.xlsx", sheetName="waste", append=TRUE, row.names=FALSE, showNA=FALSE)
 write.xlsx(data.frame(other_info), file="./detailed_data/04_output/07_other/coal_tables.xlsx", sheetName="other_info", append=TRUE, row.names=FALSE, showNA=FALSE)
 
+
+########## This part is not finished yet and it is not sure if it is even needed
+
+# ## compile a large excel sheet with all the different sheets joined
+# 
+# #first, create a sheet describing the mine in general. 
+# #there should be a unique row for each mine_fac (aggregated), but also for each sub_site.
+# #therefore, mine_fac and sub_site together form the unique primary key
+# #an empty (NA) value in sub_site indicates that the mine as a whole (aggregated) is referred to.
+# 
+# #first, create a column "sub_site" which is empty for the two sheets where no such column is specified. 
+# #this ensures that the aggregated mine_fac is referred to
+# general <- add_column(general, sub_site = NA, .after = "mine_fac")
+# sheet_min <- add_column(sheet_min, sub_site = NA, .after = "mine_fac")
+# 
+# 
+# #second, construct a table with a row for each subsite, as well as for each mine aggregated
+# #as specified above, the aggregated mine is meant when sub_site is empty (NA)
+# 
+# #include info from sheet general in sheet sub_site
+# sub_sites_additional_info <- left_join(sub_sites, 
+#                                        general %>% select(., mine_fac, country, alphanumiso, state, region, 
+#                                                           province, district, sector, location_municipality, comment, source_id),
+#                                        by = "mine_fac",
+#                                        suffix = c(".general", ".sub_site"))
+# 
+# #then join the sheet general and sheet sub_site
+# describing_table <- full_join(general, sub_sites_additional_info, 
+#                                 by=c("mine_fac","sub_site", "country", "alphanumiso", "state", "region", "province", "district", "sector", 
+#                                      "location_municipality", "latitude", "longitude", "surface_area", "unit_surface_area", "claim_concession_area", 
+#                                      "unit_concession_area", "mine_or_processing", "commodities_products", "production_start", "production_end", 
+#                                      "activity_status", "year_activity_status"), 
+#                                 suffix = c(".general", ".sub_site"))
+# 
+# 
+# describing_table
+# filter(describing_table, mine_fac == "Tuhup")
+# 
+# #third, join the mine-describing table with the other tables
+# coal_tables_joined <- full_join(describing_table, sheet_min, by = c("mine_fac", "sub_site"))
+# coal_tables_joined
+# 
+# 
+
+
