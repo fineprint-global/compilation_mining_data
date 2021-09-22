1a2a6c727f2a37c5dc9050c3bc590152f2afee57
SimonJasansky <simon.jasansky@wu.ac.at>
2021-09-22 16:41:41 +0000

    Updated detailed_data_mining.xlsx Included new check in 03_intermediate_check.Rmd to check for double entries of coordinates Updated 05_coverage_coordinated.R to properly output maps of the coordinates Updated get_coal_tables_only.R to have all sub-sites included in sheet_general

--- detailed_data/02_scripts/coverage_other/05_coverage_coordinates.R
+++ detailed_data/02_scripts/coverage_other/05_coverage_coordinates.R
@@ -3,8 +3,6 @@
 # install.packages(c("rnaturalearth", "rnaturalearthdata"))
 # install.packages("spData")
 # install.packages("ggplot2")
-
-
 library(sf)
 library(ggplot2)
 library(rnaturalearth)
@@ -16,48 +14,32 @@ theme_set(theme_bw())
 world <- ne_countries(scale = "medium", returnclass = "sf")
 class(world)
 
-
 ### read files
-
 ## harmonized data file
 detailed <- read_rds("./detailed_data/03_intermediate/gaps_filled.rds")
 
 # sheets from detailed data
 general <- detailed$general
 sub_sites <- detailed$sub_sites
+coords_before <- sum(!is.na(general$longitude))
 
+### create combined descriptive sheet from sheet_general and sheet_sub_sites
+#first, create a column "sub_site" which is empty for the sheet general
+general <- add_column(general, sub_site = NA, .after = "mine_fac")
 
-##############################################
+#then, bind the sheet general and sheet_sub_sites "beneath" each other
+general <- bind_rows(general, sub_sites)
 
-# get coordinates of all mines and processing facilities
-mines <- select(general, c(mine_fac, longitude, latitude, commodities_products, mine_or_processing))
+# the only column that has a different naming in the two sheets but refers to the same is:
+# mining_facility_types (in sheet_general) and mine_types (in sheet_sub_sites).
+# therefore, they are put together into one column, named mining_facility_types
+general$mining_facility_types <- coalesce(general$mining_facility_types, general$mine_types)
+general <- select(general, -mine_types)
+mines <- general[!is.na(general$longitude),]
 
-
-# also integrate the case where the mine in sheet general does not have coordinates, but the sub_sites do
-sub_sites <- select(sub_sites, mine_fac, sub_site, longitude, latitude)
-
-
-colnames(sub_sites) <- c("mine_fac", "sub_site","longitude_sub", "latitude_sub")
-sub_sites <- sub_sites[!is.na(sub_sites$longitude_sub),]
-mines <- left_join(mines, sub_sites, by = "mine_fac")
-sub_sites$longitude_sub
-mines$longitude
-mines$longitude_sub
-
-#if sheet_gen does not have coordinates, fill up from sheet_sub_sites
-mines %>% mutate(longitude = coalesce(longitude, longitude_sub))
-mines %>% mutate(latitude = coalesce(latitude, latitude_sub))
-
-mines %>%
-  mutate(longitude = 
-           longitude %>% 
-           is.na %>%
-           ifelse(longitude_sub, longitude))
-
-### this does not work yet!!! ###########################
-
-
-#delete possible double entered mines7sub_sites
+#check how many coordinates got added by the addition of sub_sites
+coords_after <- nrow(mines)
+coords_after - coords_before
 
 
 # come up with the commodities the mines produce, based on the column commodities_products specified in sheet general
@@ -76,7 +58,6 @@ mines$mine_or_processing_distinct <-  ifelse(grepl("Mine", mines$mine_or_process
 processing_fac <- filter(mines, mine_or_processing_distinct == "Processing")
 mines <- filter(mines, mine_or_processing_distinct == "Mine")
 
-# plot the coordinates
 
 #create sf objects out of the coordinates tibbles, for each category of commodity
 coal_sf <- st_as_sf(filter(mines, commodities_distinct == "Coal"), coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
@@ -90,6 +71,12 @@ other_sf <- st_as_sf(filter(mines, commodities_distinct == "Other"), coords = c(
 processing_fac <- st_as_sf(processing_fac, coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
 
 
+### create plots
+
+#plot for the whole world with all commodities
+pdf(file = "./detailed_data/04_output/05_coverage/coverage_spatial/coverage_coordinates.pdf",   
+    width = 17, 
+    height = 9)
 
 ggplot(data = world) +
   geom_sf() +
@@ -104,6 +91,38 @@ ggplot(data = world) +
   geom_sf(data = processing_fac, size = 2, shape = 23, fill = "cornflowerblue") +
   coord_sf(expand = FALSE)
 
+dev.off()
+
+# plot for the whole world with only coal 
+pdf(file = "./detailed_data/04_output/05_coverage/coverage_spatial/coverage_coordinates_coal.pdf",   
+    width = 17, 
+    height = 9) 
+
 ggplot(data = world) +
   geom_sf() +
-  geom_sf(data = coal_sf, size = 3, shape = 24, fill = "black") +   coord_sf(expand = FALSE)
+  geom_sf(data = coal_sf, size = 2, shape = 24, fill = "black") +
+  coord_sf(expand = FALSE)
+
+dev.off()
+
+
+# plot for only one country with all commodities (enter coordinates for the resp. country)
+
+# ggplot(data = world) +
+#   geom_sf() +
+#   geom_sf(data = coal_sf, size = 3, shape = 24, fill = "black") +
+#   geom_sf(data = iron_sf, size = 3, shape = 24, fill = "blue") +
+#   geom_sf(data = bauxite_sf, size = 3, shape = 24, fill = "red3") +
+#   geom_sf(data = gold_sc, size = 3, shape = 24, fill = "gold") +
+#   geom_sf(data = copper_sf, size = 3, shape = 24, fill = "darkorange") +
+#   geom_sf(data = pgm_sf, size = 3, shape = 24, fill = "cyan") +
+#   geom_sf(data = poly_sf, size = 3, shape = 24, fill = "darkgrey") +
+#   geom_sf(data = other_sf, size = 3, shape = 24, fill = "white") +
+#   geom_sf(data = processing_fac, size = 2, shape = 23, fill = "cornflowerblue") +
+#   coord_sf(xlim = c(-129, -90), ylim = c(0, 50), expand = FALSE)
+
+# known coordinates
+# Indonesia: xlim = c(90, 145), ylim = c(-15, 5)
+
+
+
