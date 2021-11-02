1d1f0606866c5c6b7c1d97ecc57e1285faf347a9
SimonJasansky <simon.jasansky@wu.ac.at>
2021-11-02 13:58:27 +0000

    minor adjustment of get_coal_tables_only.R

--- 02_scripts/01_detailed_data/other/get_coal_tables_only.R
+++ 02_scripts/01_detailed_data/other/get_coal_tables_only.R
@@ -1,4 +1,4 @@
-# Export only the relevant tables with coal mining for the World Bank
+# Export only the relevant tables with coal mining
 
 library(tidyverse)
 library(xlsx)
@@ -14,9 +14,6 @@ sheet_min <- detailed$minerals_ores_conce %>%
   filter(., type_mineral == "Clean coal") %>% 
   select(c(mine_fac, type_mineral, min_ore_con, year, unit, value, amount_sold, comment, source_id))
 
-# add the variable mine_id to sheet_min (in this case it is just identical to mine_fac)
-sheet_min$mine_id <- sheet_min$mine_fac
-sheet_min <- relocate(sheet_min, mine_id, .before = "mine_fac")
 
 # get list of all mines that produce coal and combine it with list of mines that are only entered in sheet general to produce coal, 
 # but which do not have production
@@ -26,9 +23,8 @@ mine_list <- union(mine_list, mine_list_general$mine_fac)
 
 
 general <- general %>% 
-  filter(., mine_fac %in% mine_list) 
+  filter(., mine_fac %in% mine_list)
 
-  
 waste <- detailed$waste %>% 
   filter(., mine_fac %in% mine_list) %>%
   select(!c(commodity, production_share))
@@ -40,17 +36,15 @@ reserves <- detailed$capacity_reserves %>%
   select(!c(processing_capacity_year,	processing_capacity_unit,	processing_capacity_value, reserves_share,
             grade_unit,	grade, reserves_commodity_unit, reserves_commodity_value))
 
-#join production with coordinates from sheet general
-coal_production <- merge(sheet_min, select(general, mine_id, country, alphanumiso, commodities_products, mining_facility_types, geom), by = "mine_id")
 
 # write sheet general as geopackage
-st_write(coal_production, "./04_output/01_detailed_data/07_other/coal_production_georeferenced.gpkg", append = FALSE)
+write_delim(sheet_min, "./04_output/01_detailed_data/07_other/coal_production_georeferenced.csv", delim = ";")
 
 # write sheet general as geopackage
 st_write(general, "./04_output/01_detailed_data/07_other/general_coal.gpkg", append = FALSE)
 
-# # write production data as excel file
-# write.xlsx(data.frame(sheet_min), file="./04_output/01_detailed_data/07_other/coal_tables.xlsx", sheetName="sheet_min", row.names=FALSE, showNA=FALSE)
+
+# # write other sheets (reserves, waste, other_info) to excel if needed
 # write.xlsx(data.frame(reserves), file="./04_output/01_detailed_data/07_other/coal_tables.xlsx", sheetName="reserves", append=TRUE, row.names=FALSE, showNA=FALSE)
 # write.xlsx(data.frame(waste), file="./04_output/01_detailed_data/07_other/coal_tables.xlsx", sheetName="waste", append=TRUE, row.names=FALSE, showNA=FALSE)
 # write.xlsx(data.frame(other_info), file="./04_output/01_detailed_data/07_other/coal_tables.xlsx", sheetName="other_info", append=TRUE, row.names=FALSE, showNA=FALSE)
